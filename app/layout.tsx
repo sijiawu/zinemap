579130ec764a016dcd62adeda0997399530047d1
sijiawu <sijiawu95@gmail.com>
2026-04-03 20:39:00 +1100

    add clarity minus mouseflow

--- app/layout.tsx
+++ app/layout.tsx
@@ -78,14 +78,17 @@ export default function RootLayout({
           }}
         />
         <Script
-          id="mouseflow-init"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{ __html: "window._mfq = window._mfq || [];" }}
-        />
-        <Script
-          id="mouseflow"
-          strategy="beforeInteractive"
-          src="https://cdn.mouseflow.com/projects/77b216ba-4cd5-46e6-a109-e8811cdf10d9.js"
+          id="microsoft-clarity"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "w5ukq9nmuu");
+            `,
+          }}
         />
       </head>
       <body>
